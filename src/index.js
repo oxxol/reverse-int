5ab987896965619d6725fd366ee1503eded201ee
Oxana Oleynikova <oleynikova.oxana@gmail.com>
2022-01-10 03:12:32 +0100

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let mod = String(Math.abs(n));
+    let result = '';
+    for (let i = 0; i < mod.length; i += 1) {
+        result = `${mod[i]}${result}`;
+    }
+    return Number(result);
+};
